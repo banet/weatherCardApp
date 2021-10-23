59120dc1aef3304c05b5f07461ee8e60a03abbad
bane <bane.tepavcevic@gmail.com>
2021-10-23 21:37:05 +0200

    updated UI part adding updateUI function responsible for getting data and dieplaying in the card

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,9 @@
 
 
 const cityForm = document.querySelector('form')
+const card = document.querySelector('.card')
+const details = document.querySelector('.details')
+
 
 // The idea is just to call  getCity, getWeather function inside updateCity
 const updateCity = async (city) => {
@@ -28,6 +31,32 @@ function addCityApi(e) {
     // update the UI with new city name with new function and add then(), cathc()
 
     updateCity(city)
-    .then( data => console.log(data))
+    .then( data => updateUI(data))
     .catch(err => console.log(err))
+}
+
+
+
+// Update UI 
+//Wil be responsilbe for taking in some data and output in it ti the browser using these referance
+const updateUI = (data) => {
+
+    const cityDets = data.cityDets
+    const weather = data.weather
+
+    // Update details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+        `
+
+        // remove d-none class if present
+
+        if(card.classList.contains('d-none')) {
+            card.classList.remove('d-none')
+        } 
 }
